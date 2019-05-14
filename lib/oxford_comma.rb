f9b33cf490a8f11c3b00285525a85ce6ab8cec9f
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-14 18:41:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     str =array[0]
    #return str
   elsif array.length ==2
-    str =array[0]<<" and"<<array[1]
+    str =array[0]<<" and"<< array[1]
   else
     str =array.fetch(-1)
     str2 = "and"<<" #{str}"
