32a7693ac13a530acf80ecf9c364f84744b4a0a6
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-14 18:36:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,12 @@
 def oxford_comma(array)
-  str =array.fetch(-1)
-  str2 = "and"<<" #{str}"
-  array.pop
-  array<<str2
-  array.join(", ")
+  if array.length ==1
+    return array(0)
+  else
+    str =array.fetch(-1)
+    str2 = "and"<<" #{str}"
+    array.pop
+    array<<str2
+    array.join(", ")
+  end
 end
  
