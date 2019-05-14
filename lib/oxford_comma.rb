3c4965ab631c4eddd6b0890167721c7c753fa0e4
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-14 18:08:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,5 @@
 def oxford_comma(array)
-  str =""
-  counter =0
-  array.each do |fruit|
-    str<<"#{fruit}"
-    counter +=1
-  end
-  str
+  last = array[-1]
+  array.pop
+  array[-1] = "and " << #{last}
 end
